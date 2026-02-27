5583083a1f8ff4e665592bde9ff563f91ab0576d
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-27 22:58:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
 //your JS code here. If required.
+const userInput = document.getElementById("ip");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+const promise1 =() => {
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			resolve(userInput.value);
+		},2000)
+	})
+}
+const promise2 =(data) => {
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			
+			resolve(data * 2);
+		},2000)
+	})
+}
+const promise3 =(data) => {
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			
+			resolve(data - 3);
+		},1000)
+	})
+}
+const promise4 = (data) => {
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			
+			resolve(data/2);
+		},1000)
+	})
+}
+const promise5 = (data) => {
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+		
+			resolve(data + 10);
+		},1000)
+	})
+}
+
+function calculation(){
+	promise1()
+		.then((res) => {
+		output.textContent = res;
+		return promise2(res);
+	}).then((res) =>{
+		output.textContent = res;
+		return promise3(res);
+	}).then((res) =>{
+		output.textContent = res;
+		return promise4(res);
+	}).then((res) => {
+		  output.textContent = res;
+			return promise5(res);
+			
+	}).then((res) =>{
+    output.textContent = res;
+});
+	
+}
+
+
+
